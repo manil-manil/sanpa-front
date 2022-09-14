a78f650bbd3c97c260cce7f792f7ebc21b6e7caf
Uigyo Kim <kcdmlry@secondspace.kr>
2022-09-15 00:53:37 +0900

    INIT: nextjs + ts + recoil + yarn berry + emotion

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import TreePage from "../src/components/TreePage";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <TreePage />
+    </div>
+  );
+};
+
+export default Home;
